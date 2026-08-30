965bd7240c58dd9bd38856bb876c6ee5b573cd90
Kirill <dom.zavadski@gmail.com>
2026-08-30 18:33:41 +0300

    Tests: fixed issue with ordering

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from app import create_app
 from models import db
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="module", autouse=True)
 def app():
     app = create_app()
     with app.app_context():
