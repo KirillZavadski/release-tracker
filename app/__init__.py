7eebec4c78e636c3760a944f417ffee5fbfe26bd
Kirill <dom.zavadski@gmail.com>
2026-09-12 23:40:11 +0300

    delete tables and all migrations

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,8 @@ def create_app():
     app = Flask(__name__)
 
     basedir = os.path.abspath(os.path.dirname(__file__))
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    project_root = os.path.dirname(basedir)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(project_root, 'app.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
