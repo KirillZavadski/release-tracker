e920be6d38b9433bf0b847ad35a7463b81275c45
Kirill <dom.zavadski@gmail.com>
2026-09-19 21:51:02 +0300

    added enums file

---
+++ app/models/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Status_variable(Enum):
+    DRAFT = "Draft"
+    TESTING = "Testing"
+    DEPLOYED = "Deployed"
--- app/models/models.py
+++ app/models/models.py
@@ -3,14 +3,10 @@ from typing import Optional, List
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Text, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from enum import Enum
+
+from app.models.enums import Status_variable as statuses
 
 db = SQLAlchemy()
-
-class Status_variable(Enum):
-    DRAFT = "Draft"
-    TESTING = "Testing"
-    DEPLOYED = "Deployed"
 
 class Service(db.Model):
     __tablename__ = 'services'
@@ -34,7 +30,7 @@ class Release(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     version: Mapped[str] = mapped_column(String(50))
     changelog: Mapped[Optional[str]] = mapped_column(Text)
-    status: Mapped[Status_variable] = mapped_column(db.Enum(Status_variable), default=Status_variable.DRAFT)
+    status: Mapped[statuses] = mapped_column(db.Enum(statuses), default=statuses.DRAFT)
     created_at: Mapped[datetime] = mapped_column(default=datetime.now(UTC))
     
     service_id: Mapped[int] = mapped_column(ForeignKey('services.id'))
@@ -44,7 +40,7 @@ class Release(db.Model):
             'id': self.id,
             'version': self.version,
             'changelog': self.changelog,
-            'status': self.status.value if isinstance(self.status, Status_variable) else self.status,
+            'status': self.status.value if isinstance(self.status, statuses) else self.status,
             'created_at': self.created_at.isoformat() if self.created_at else None,
             'service_id': self.service_id
         }
